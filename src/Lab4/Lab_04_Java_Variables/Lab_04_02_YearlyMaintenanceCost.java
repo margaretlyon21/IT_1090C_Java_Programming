d9fefdce2934a63a75e85e4fd490ffff91368974
maggielyon <margaret.lyon21@gmail.com>
2024-05-14 13:48:43 -0400

    Creating Java Class Repository

---
+++ src/Lab4/Lab_04_Java_Variables/Lab_04_02_YearlyMaintenanceCost.java
@@ -0,0 +1,13 @@
+package Lab4.Lab_04_Java_Variables;
+
+public class Lab_04_02_YearlyMaintenanceCost {
+    public static void main(String[] args) {
+        double winterCost = 120;
+        double springCost = 400.50;
+        double summerCost = 327;
+        double fallCost = 667.7;
+        double sum = winterCost + springCost + summerCost + fallCost;
+        System.out.println("The yearly maintenence cost is " + sum + ". The winter cost was " + winterCost + ", the spring cost was " + springCost + ", the summer cost was " + summerCost + ", and the fall cost was " + fallCost);
+
+    }
+}
